f453f4cb49173d9e2f9b92cbfbcebea54f54ffcf
JhonathanALM <jalechon@espe.edu.ec>
2017-06-28 19:53:12 -0500

    Se agrego un nuevo paquete dao ".." Y se eliminaron 6 clases

---
+++ EducaT-ejb/src/main/java/ec/edu/espe/isi/dao/AlumnoFacade.java
@@ -0,0 +1,27 @@
+package ec.edu.espe.isi.dao;
+
+import ec.edu.espe.isi.model.Alumno;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * @author jhona
+ */
+@Stateless 
+public class AlumnoFacade extends AbstractFacade<Alumno> 
+{
+
+    @PersistenceContext(unitName = "ec.edu.espe.isi_EducaT-ejb_ejb_1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AlumnoFacade() {
+        super(Alumno.class);
+    }
+
+}
